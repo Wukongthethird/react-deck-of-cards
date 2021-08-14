8d2884d9e91a30bdfd9ad58a3b9d0c37e8b85c50
magus <harvestashya@gmail.com>
2021-08-13 17:04:17 -0700

    shuffling

--- src/Deckcomponent.js
+++ src/Deckcomponent.js
@@ -17,6 +17,7 @@ function DeckComponent(){
   const [isDrawing, setIsDrawing] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [playedCards, setPlayedCards] = useState([]);
+  const [isShuffling, setIsShuffling] = useState(false);
 
   // set states for  deckOfCardsId on mount
   useEffect( function getDeckWhenMounted(){
@@ -29,20 +30,36 @@ function DeckComponent(){
   },[] );
 
 
-  // Draws a card from API and updates playedCards
-  useEffect( function drawCardOnClick(){
-    async function getCard(){
-      const response = await axios.get(BASE_URL+deckOfCardsId+DRAW);
-        setPlayedCards( playedCards => [...playedCards, response.data.cards[0]]);
-    }
-    if(deckOfCardsId !==null)  getCard();
-  }, [isDrawing]);
-
-
-  function drawCard(){
-    setIsDrawing((isDrawing) => !isDrawing);
+  async function getCard(){
+    const response = await axios.get(BASE_URL+deckOfCardsId+DRAW);
+      setPlayedCards( playedCards => [...playedCards, response.data.cards[0]]);
   }
 
+  // Draws a card from API and updates playedCards
+  // useEffect( function drawCardOnClick(){
+  //   async function getCard(){
+  //     const response = await axios.get(BASE_URL+deckOfCardsId+DRAW);
+  //       setPlayedCards( playedCards => [...playedCards, response.data.cards[0]]);
+  //   }
+  //   if(deckOfCardsId !==null)  getCard();
+  // }, [isDrawing]);
+
+
+  useEffect(function shuffleDeckAPI(){
+    async function shuffle(){
+      await axios.get(BASE_URL+deckOfCardsId+"/shuffle");
+      setPlayedCards([]);
+    }
+    shuffle();
+  }, [isShuffling])
+
+  // function drawCard(){
+  //   setIsDrawing((isDrawing) => !isDrawing);
+  // }
+
+  function shuffleDeck(){
+    setIsShuffling((isShuffling) => !isShuffling);
+  }
   // useEffect( function drawOnClick(){
 
 
@@ -53,9 +70,10 @@ function DeckComponent(){
   return (
     <div>
       {playedCards.length<=51
-        ?<button onClick={drawCard}>Draw</button>
+        ?<button onClick={getCard}>Draw</button>
         :alert("Error: no cards remaining!")
       }
+      <button onClick={shuffleDeck}>reset</button>
       <CardPile playedCards={playedCards}  />
     </div>
   )
